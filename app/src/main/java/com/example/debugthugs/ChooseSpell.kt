ae3d7341941caa062c70b62974dbc8e6f2d16770
lyonsd3 <lyonsd3@wwu.edu>
2021-04-10 21:22:09 -0700

    UI for choosing spells :)

--- app/src/main/java/com/example/debugthugs/ChooseSpell.kt
+++ app/src/main/java/com/example/debugthugs/ChooseSpell.kt
@@ -2,13 +2,10 @@ package com.example.debugthugs
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
-import android.support.v4.app.Fragment
+
 class ChooseSpell : AppCompatActivity() {
 
 
@@ -23,21 +20,21 @@ class ChooseSpell : AppCompatActivity() {
         //val resultTextView: TextView = findViewById(R.id.textView)
 
         /* Wizard 1 */
-        val btn_click_me = findViewById(R.id.button) as Button
+        val btn_click_me = findViewById(R.id.wiz1) as Button
         btn_click_me.setOnClickListener{
             Toast.makeText(this@ChooseSpell, "You clicked Wizard 1", Toast.LENGTH_SHORT).show()
             //resultTextView.text = "Wiz1"
         }
 
         /* Wizard 2 */
-        val btn_click_me2 = findViewById(R.id.button2) as Button
+        val btn_click_me2 = findViewById(R.id.wiz2) as Button
         btn_click_me2.setOnClickListener{
             Toast.makeText(this@ChooseSpell, "You clicked Wizard 2", Toast.LENGTH_SHORT).show()
             //resultTextView.text = "Wiz2"
         }
 
         /* Wizard 3 */
-        val btn_click_me3 = findViewById(R.id.button3) as Button
+        val btn_click_me3 = findViewById(R.id.wiz3) as Button
         btn_click_me3.setOnClickListener{
             Toast.makeText(this@ChooseSpell, "You clicked Wizard 3", Toast.LENGTH_SHORT).show()
             //resultTextView.text = "Wiz3"
