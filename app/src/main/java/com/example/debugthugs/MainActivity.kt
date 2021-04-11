9f2475d9681b645bb4b5fea070fd57817a8aa9a4
lyonsd3 <lyonsd3@wwu.edu>
2021-04-10 23:08:34 -0700

    New menu for new project

--- app/src/main/java/com/example/debugthugs/MainActivity.kt
+++ app/src/main/java/com/example/debugthugs/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.debugthugs
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +18,22 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-    public fun goToChooseSpell(view: View) {
-        val sg: Intent = Intent(this, ChooseSpell::class.java)
+    fun goToTTT(view: View) {
+        val sg: Intent = Intent(this, TicTacToe::class.java)
         startActivity(sg)
     }
 
+    fun goToRPS(view: View) {
+        val sg: Intent = Intent(this, RockPaperScissors::class.java)
+        startActivity(sg)
+    }
 
+    fun goToCoinF(view: View) {
+        val sg: Intent = Intent(this, CoinFlip::class.java)
+        startActivity(sg)
+    }
 }
+
 
 
 
